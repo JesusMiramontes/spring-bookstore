814930df48b8668bb4d6367f5c058ae0bdc25848
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-08 11:03:27 -0500

    Created book controller

---
+++ src/main/java/com/miramontes/bookstore/controllersBookController.java
@@ -0,0 +1,22 @@
+package com.miramontes.bookstore;
+
+import com.miramontes.bookstore.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controllersBookController {
+
+    private final BookRepository bookRepository;
+
+    public controllersBookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
